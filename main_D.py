157b5b6f4a757eeb886cbc17ad3f5a4d2f5517b3
Omestya <113118725+Omestya@users.noreply.github.com>
2025-03-15 09:52:09 +0200

    www

--- main_D.py
+++ main_D.py
@@ -1,6 +1,10 @@
 import module_Demydchyk
+import module_dovholiuk
 a = 10
 b = 5
 
 print("Сума", module_Demydchyk.add(a, b))
 print("Різниця", module_Demydchyk.sub(a, b))
+
+print("Ділення: ", module_Demydchyk.dilen(a, b))
+print("Множення", module_Demydchyk.mnog(a, b))
