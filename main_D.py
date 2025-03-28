089c72c687e512aa0b4cf0d7647ad271e3f4fa75
Demydchyk <demidcikkata12@gmail.com>
2025-03-28 23:28:54 +0200

    Update main_D.py

--- main_D.py
+++ main_D.py
@@ -3,8 +3,8 @@ import module_dovholiuk
 a = 10
 b = 5
 
-print("Сума", module_Demydchyk.add(a, b))
-print("Різниця", module_Demydchyk.sub(a, b))
+print("Функція 1 Демидчик: ", module_Demydchyk.f1(a, b))
+print("Функція 2 Демидчик: ", module_Demydchyk.f2(a, b))
 
 print("Ділення: ", module_dovholiuk.dilen(a, b))
 print("Множення", module_dovholiuk.mnog(a, b))
