16c154eb717ed5ecb4b23ea24f37a07162d19744
Valentin <126572089+KoCia4ioK@users.noreply.github.com>
2026-02-27 09:31:58 +0200

    the 2nd update

--- Мельничук Артур/project/project for 4 ppl.py
+++ Мельничук Артур/project/project for 4 ppl.py
@@ -1,1 +1,14 @@
+#Імпорт візуальної частини
 from tkinter as tk
+
+#Два поля введення чисел
+A =int(input(" " : ))
+B =int(input(" " :  ))
+
+def plus():
+
+def minus():
+
+def divide():
+
+def times():
