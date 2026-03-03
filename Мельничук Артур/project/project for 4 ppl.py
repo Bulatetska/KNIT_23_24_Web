c3bc1c85f95cae8f0f9bd288a798ab688eaa07a0
yevdokimov681-lab <vevdokimov681@gmail.com>
2026-03-03 18:55:29 +0200

    Update project for 4 ppl.py

--- Мельничук Артур/project/project for 4 ppl.py
+++ Мельничук Артур/project/project for 4 ppl.py
@@ -6,11 +6,17 @@ from random as rn
 A =int(input(" " : ))
 B =int(input(" " :  ))
 
+#Функція котра робить додаваня
 def plus():
+    return A + B
 
+#Функція котра робить відніманя
 def minus():
+    return A - B
 
+#Функція котра робить діленя
 def divide():
+    return A / B
 
 #Функція котра робить множення
 def times():
