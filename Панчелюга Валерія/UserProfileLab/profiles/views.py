8a03fc1b48df4927aed8b87b0dc69bc0e5898d6e
Valeriia <valeriyap1106@gmail.com>
2026-03-04 17:19:33 +0200

    с

--- Панчелюга Валерія/UserProfileLab/profiles/views.py
+++ Панчелюга Валерія/UserProfileLab/profiles/views.py
@@ -5,9 +5,8 @@ def home(request):
     return HttpResponse("Система профілів користувачів")
 def users(request):
     return HttpResponse("Список користувачів: Іван, Марія, Олег")
-def users_detail(request):
-    return HttpResponse(f"Користувач: {users}")
-def users_orders(request):
-    return HttpResponse(f"Замовлення користувача: {users}")
+def user_detail(request, username):
+    return HttpResponse(f"Користувач: {username}")
+def user_orders(request, username):
+    return HttpResponse(f"Замовлення користувача: {username}")
 
-# Create your views here.
