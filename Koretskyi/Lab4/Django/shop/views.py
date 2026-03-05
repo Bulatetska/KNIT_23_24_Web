66e6fc3ebba28755394c63b5a55cf021fae2e693
06878877-sasha <sashkakorecki@gmail.com>
2026-03-05 12:32:03 +0200

    c

--- Koretskyi/Lab4/Django/shop/views.py
+++ Koretskyi/Lab4/Django/shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def products(request):
     items = [
-        {"name": "Ноутбук", "price": 1500, "stock": 10},
+        {"name": "Ноутбук", "price": 1500, "stock": 0},
         {"name": "Смартфон", "price": 800, "stock": 15},
         {"name": "Навушники", "price": 200, "stock": 50},
     ]
