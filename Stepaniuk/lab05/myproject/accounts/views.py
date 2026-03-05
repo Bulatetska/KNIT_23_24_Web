9792e9dd524c8bd8c8d2976647aedc887e565831
Stepaniuk-Ole <ggcsgo127@gmail.com>
2026-03-05 12:29:36 +0200

    lab05

---
+++ Stepaniuk/lab05/myproject/accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    message = ""
+
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            message = "Успішна реєстрація"
+        else:
+            message = "Дані некоректні"
+    else:
+        form = RegistrationForm()
+
+    return render(request, "register.html", {"form": form, "message": message})
