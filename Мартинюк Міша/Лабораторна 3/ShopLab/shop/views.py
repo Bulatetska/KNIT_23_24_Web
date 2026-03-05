9d1bd1e3b0ef269c85a735ec99d4823f9d85f14e
Misha <martuniykmisha@gmail.com>
2026-03-05 11:13:29 +0200

    c

---
+++ Мартинюк Міша/Лабораторна 3/ShopLab/shop/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Ласкаво просимо до нашого магазину")
+
+
+def sale(request):
+    return HttpResponse("Акційні товари магазину")
+
+
+def category(request, category):
+    return HttpResponse(f"Категорія: {category}")
+
+
+def product(request, category, product):
+    return HttpResponse(f"Товар: {product} у категорії {category}")
