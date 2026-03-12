77132b0eea3f831ef9c81c0e53726ee2ff6a6ce0
lubochkov_Yevhen <lubochkov.jevgen1701@gmail.com>
2026-03-12 12:58:40 +0200

    4.2

--- Django/shop/urls.py
+++ Django/shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.products, name='products'),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- Django/shop/views.py
+++ Django/shop/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import render
 
-def products(request):
-    items = [
-        {"name": "Ноутбук", "price": 1500, "stock": 10},
-        {"name": "Смартфон", "price": 800, "stock": 15},
-        {"name": "Навушники", "price": 200, "stock": 0},
-    ]
-    return render(request, "products.html", {"items": items})
+def index(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
