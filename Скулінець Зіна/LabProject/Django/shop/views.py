363cf86bee6f3afd09f7d8d3b471be63c0628b80
zinaskulinets-design <zinaskulinets@gmail.com>
2026-03-18 22:05:21 +0200

    с

--- Скулінець Зіна/LabProject/Django/shop/views.py
+++ Скулінець Зіна/LabProject/Django/shop/views.py
@@ -5,6 +5,6 @@ def products(request):
         {"name": "Ноутбук", "price": 1500, "stock": 10},
         {"name": "Смартфон", "price": 800, "stock": 15},
         {"name": "Навушники", "price": 200, "stock": 50},
-        {"name": "Планшет", "price": 300, "stock": 0},  # Додав товар із 0 для перевірки
+        {"name": "Планшет", "price": 400, "stock": 0},
     ]
     return render(request, "products.html", {"items": items})
