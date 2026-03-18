5a3e0dc1383dbd2ce90892dc9c0d1de7c2b2666b
lubochkov_Yevhen <lubochkov.jevgen1701@gmail.com>
2026-03-18 23:23:37 +0200

    lab 5

--- Django/shop/urls.py
+++ Django/shop/urls.py
@@ -1,7 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from shop import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
+    path('', views.index),
+    path('about/', views.about),
 ]
