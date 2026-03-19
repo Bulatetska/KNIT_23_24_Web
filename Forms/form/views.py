8f2aa10a02bd1d34c8cc6393585e6888f1e3e9fe
RombinTrinity <krasilcukbogdan4@gmail.com>
2026-03-19 13:01:16 +0200

    Completed task with RegistrationForm class

--- Forms/form/views.py
+++ Forms/form/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .custom_forms import RegistrationForm
 
-# Create your views here.
+def index(request):
+    registration_form = RegistrationForm()
+    if request.method == 'POST':
+        registration_form = RegistrationForm(request.POST)
+        message = ""
+        if registration_form.is_valid():
+            message = f"Успішна реєстрація"
+        else:
+            message = "Дані некоректні"
+        return render(request, 'index.html', {'form': registration_form, 'message': message})
+
+    return render(request, 'index.html', {"form": registration_form})
