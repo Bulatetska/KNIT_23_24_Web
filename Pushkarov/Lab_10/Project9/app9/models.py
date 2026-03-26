cd328449cbbb8b0a4033de59cbb8aeeca345fe64
Сергій Пушкарьов <pushkarovsergey@gmail.com>
2026-03-26 12:47:24 +0200

    Add Lab_10

---
+++ Pushkarov/Lab_10/Project9/app9/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Student(models.Model):
+    Name = models.CharField(max_length=100)
+    Year = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    Student_ID = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.Name
+class Course(models.Model):
+ Course_Name = models.CharField(max_length=100)
+ Duration_Hours = models.IntegerField(validators=[MinValueValidator(1)])
+ def __str__(self):
+  return self.Course_Name
