0e1d4423f263e4b8a5beec970c17340c57493efb
RombinTrinity <krasilcukbogdan4@gmail.com>
2026-03-26 12:35:52 +0200

    Completed task with FeedbackForm class

--- FeedbackForm/userdata/views.py
+++ FeedbackForm/userdata/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .custom_form import FeedbackForm
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            with open('feedback.txt', 'a') as f:
+                f.write(f"Full Name: {data['full_name']}\n")
+                f.write(f"Email: {data['email']}\n")
+                f.write(f"Message: {data['message']}\n")
+                f.write(f"Rating: {data['rating']}\n")
+
+            return render(request, 'index.html', {'form': FeedbackForm(), 'success': True})
+    else:
+        form = FeedbackForm()
+
+    return render(request, 'index.html', {'form': form})
