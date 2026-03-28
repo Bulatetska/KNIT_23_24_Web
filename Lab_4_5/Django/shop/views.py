fb0812f357b60409ee00c6bb79f3e653894ed44d
Volody <bondarchuk0volody@gmail.com>
2026-03-28 12:44:29 +0200

    Лаби

---
+++ Lab_4_5/Django/shop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+ 
+def products(request):
+    items = [
+        {"name": "Ноутбук", "price": 1500, "stock": 0},
+        {"name": "Смартфон", "price": 800, "stock": -1},
+        {"name": "Навушники", "price": 200, "stock": 50},
+    ]
+    return render(request, "products.html", {"items": items})
+def index(request):
+    return render(request, "index.html")
+
+def about(request):
+
+    return render(request, "about.html")
+ 
