7948cc6b9b4771a035948a167071c593f38b9beb
Oleksii <ostapovetsoleksii@gmail.com>
2026-03-31 18:05:35 +0300

    6
    6

---
+++ Lab6/Lab6_project/Lab6_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Lab6AppConfig(AppConfig):
+    name = 'Lab6_app'
---
+++ Lab6/Lab6_project/Lab6_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class student_info(models.Model):
+    full_name = models.CharField(max_length=150)
+    year_of_study = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    student_id = models.CharField(max_length=10)
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    duration_hours = models.DurationField()
+
