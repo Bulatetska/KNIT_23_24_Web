faa210ebd12168cf591390f842b29c5954883e62
KonstantinTkach <ironing.kostya7092@gmail.com>
2026-04-01 20:38:06 +0300

    Додано лабораторні роботи Костянтина Ткача

---
+++ Konstantin Tkach/Lab_3/CourseLab/CourseLab/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('courses/', include('coursed.urls')),  # або 'courses.urls' залежно від назви
+]
---
+++ Konstantin Tkach/Lab_3/CourseLab/coursed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoursedConfig(AppConfig):
+    name = 'coursed'
