7ccfac4edcf19cd54e495046a6005f309395b74a
a-yakovets <yakovets.anya154@gmail.com>
2026-04-01 10:46:56 +0300

    lab5

--- Yakovets_KNIT24/Project_test/urls.py
+++ Yakovets_KNIT24/Project_test/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from shop.views import products 
+from shop import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', products),  
+    path('admin/', admin.site.urls),           
+    path('', views.index, name='index'),        
+    path('about/', views.about, name='about'), 
 ]
