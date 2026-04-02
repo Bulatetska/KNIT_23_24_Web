ae84e44b2729ad9282d1ae5f6270c296d074f492
RombinTrinity <krasilcukbogdan4@gmail.com>
2026-04-02 11:58:21 +0300

    Commit

--- SQLiteProject/sql/models.py
+++ SQLiteProject/sql/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=150)
+    year_of_study = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    student_id = models.IntegerField(max_length=10, primary_key=True)
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    duration_hours = models.IntegerField(validators=[])
