4a29ccea794098dc33fe0073877230506c82cc7b
RombinTrinity <krasilcukbogdan4@gmail.com>
2026-04-02 12:12:51 +0300

    Added functions to operate with database

--- SQLiteProject/sql/views.py
+++ SQLiteProject/sql/views.py
@@ -1,2 +1,14 @@
 from django.shortcuts import render
+from .models import Student
 
+student_1 = Student.objects.create(full_name="Олександр Іваненко", year_of_study="2", student_id="S123456789")
+student_2 = Student.objects.create(full_name="Марія Петренко", year_of_study="3", student_id="S987654321")
+
+def update_student(student_id, year_of_study):
+    student = Student.objects.filter(student_id=student_id).update(year_of_study=year_of_study)
+
+def delete_student():
+    Student.objects.filter(year_of_study="5").delete()
+
+def all_students():
+    return Student.objects.order_by('-year_of_study')
