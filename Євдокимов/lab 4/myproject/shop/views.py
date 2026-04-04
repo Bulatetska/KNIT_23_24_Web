20737dffb6c20cdc3c0f1644628601497e6056e7
yevdokimov681-lab <vevdokimov681@gmail.com>
2026-04-04 22:36:01 +0300

    4 lab

---
+++ Євдокимов/lab 4/myproject/shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def products(request):
+    items = [
+        {"name": "Ноутбук", "price": 1500, "stock": 10},
+        {"name": "Смартфон", "price": 800, "stock": 15},
+        {"name": "Навушники", "price": 200, "stock": 0},  # для перевірки "Немає в наявності"
+    ]
+    return render(request, "products.html", {"items": items})
