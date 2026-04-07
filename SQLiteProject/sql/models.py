1d45ee798d4f79127f7ad509a8e78678197a0789
RombinTrinity <krasilcukbogdan4@gmail.com>
2026-04-07 09:42:38 +0300

    Added few more functions to operate with database

--- SQLiteProject/sql/models.py
+++ SQLiteProject/sql/models.py
@@ -12,6 +12,12 @@ class Student(models.Model):
     )
     student_id = models.CharField(max_length=10, primary_key=True)
 
+    def __str__(self):
+        return f"[{self.student_id}, {self.full_name}, {self.year_of_study}]"
+
 class Course(models.Model):
     course_name = models.CharField(max_length=100)
     duration_hours = models.IntegerField(validators=[])
+
+    def __str__(self):
+        return f"[{self.course_name}, {self.duration_hours}]"
