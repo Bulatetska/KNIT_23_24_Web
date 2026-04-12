82b3af2bead0327ee3f3b9a3178112bf373ddaf6
yevdokimov681-lab <vevdokimov681@gmail.com>
2026-04-12 13:18:07 +0300

    lab 4 lab 6

---
+++ Євдокимов/lab 6/myproject/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            message = "Успішна реєстрація"
+        else:
+            message = "Дані некоректні"
+    else:
+        form = RegistrationForm()
+        message = ""
+
+    return render(request, "register.html", {
+        "form": form,
+        "message": message
+    })
