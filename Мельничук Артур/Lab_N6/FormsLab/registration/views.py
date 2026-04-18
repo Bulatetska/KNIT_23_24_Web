654318ff47ea81d24bd96f92cb06ff7403e58c0c
ArturMel321 <artyrka2007minecraft1@gmail.com>
2026-04-18 17:26:17 +0300

    6 laba

---
+++ Мельничук Артур/Lab_N6/FormsLab/registration/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            message = "Успішна реєстрація"
+        else:
+            message = "Дані некоректні"
+        return render(request, "register.html", {"form": form, "message": message})
+    else:
+        form = RegistrationForm()
+        return render(request, "register.html", {"form": form})
