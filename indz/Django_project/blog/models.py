2bded9edd99bdbb6a59da94245e164c16a1b1fae
Oleksii <ostapovetsoleksii@gmail.com>
2026-04-21 16:10:56 +0300

    Ostapovets
    changed by Ostapovets

--- indz/Django_project/blog/models.py
+++ indz/Django_project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at'] # Пункт 7: Нові зверху
+
+    def str(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
