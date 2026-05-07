9524b66e7d6b4f62f61579c89971206bcef5394a
Dragoneron <55554101+Dragoneron@users.noreply.github.com>
2026-05-08 01:08:57 +0300

    new 2

--- Шваліковський Артем/labproject 3/UserProfileLab/JsonResponseLab/api/views.py
+++ Шваліковський Артем/labproject 3/UserProfileLab/JsonResponseLab/api/views.py
@@ -1,23 +1,37 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import JsonResponse
 
-def users(request):
+users = {
+    "Іван": {
+        "name": "Іван",
+        "age": 25,
+        "orders": ["Ноутбук", "Смартфон"]
+    },
+
+    "Марія": {
+        "name": "Марія",
+        "age": 30,
+        "orders": ["Навушники", "Планшет"]
+    }
+}
+
+def users_list(request):
+
     data = [
         {"name": "Іван", "age": 25},
         {"name": "Марія", "age": 30}
-        {"name": "Олег", "age": 24}
-        {"name": "Артур", "age": 18}
-        {"name": "Артем", "age": 19}
     ]
+
     return JsonResponse(data, safe=False)
 
+
 def user_detail(request, name):
-    data = {
-        "name":Іван ,
-        "age": 25,
-        "orders": ["Ноутбук", "Смартфон"]
-    }
-    return JsonResponse(data)
+
+    user = users.get(name)
+
+    if user:
+        return JsonResponse(user)
+
+    return JsonResponse(
+        {"error": "Користувача не знайдено"},
+        status=404
+    )
